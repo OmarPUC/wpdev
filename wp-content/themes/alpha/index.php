287534010f5a7b06f30daf1871a1b3c02fcbdfbd
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-03-24 22:55:16 +0600

    bootstraping

--- wp-content/themes/alpha/index.php
+++ wp-content/themes/alpha/index.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php bloginfo("name"); ?>
+    </title>
+</head>
+<body>
+    
 <?php
 
-$title = get_bloginfo("name");
-echo change_case($title);
+    while(have_posts()){
+        the_post();
+
+        echo "<h2>";
+        the_title();
+        echo "</h2>";
+    }
+?>
+
+</body>
+</html>
