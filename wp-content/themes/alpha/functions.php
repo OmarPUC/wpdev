f349011d159119634b5223fc3fb8375503d9a8a9
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-03-25 20:48:39 +0600

    Sidebar Widgets register in single post view

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -7,8 +7,25 @@ function alpha_bootstrapping(){
 }
 add_action("after_setup_theme","alpha_bootstrapping");
 
+
 function alpha_assets(){
     wp_enqueue_style("alpha",get_stylesheet_uri());
     wp_enqueue_style("bootstrap","//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 add_action("wp_enqueue_scripts","alpha_assets");
+
+
+function alpha_sidebar(){
+    register_sidebar(
+        array(
+            'name'          => __( 'Single Post Sidebar', 'alpha' ),
+            'id'            => 'sidebar-1',
+            'description'   => __( 'Right sidebar.','alpha' ),
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+}
+add_action("widgets_init","alpha_sidebar");
