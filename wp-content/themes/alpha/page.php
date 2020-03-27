84f790afc2150c314a38d6baef02e3a3b9203c53
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-03-27 15:57:06 +0600

    file organize

--- wp-content/themes/alpha/page.php
+++ wp-content/themes/alpha/page.php
@@ -1,4 +1,67 @@
-<?php
+<?php get_header(); ?>
+<body <?php body_class(); ?> >
 
-get_template_part("pt","one");
-get_template_part("pttwo");
+<?php get_template_part("/template-parts/common/hero"); ?>
+
+            <div class="posts">
+                <?php 
+                    while(have_posts()):
+                        the_post();
+                ?>
+                <div class="post" <?php post_class(); ?>>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 offset-md-1">
+                                <h2 class="post-title text-center">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <p class="text-center">
+                                    <strong>
+                                        <?php the_author(); ?>
+                                    </strong><br/>
+                                    <?php echo get_the_date(); ?>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-10 offset-md-1">
+                                <p>
+                                    <?php
+
+                                    //Featherlight does not work in gutenburg, see it later
+                                        if(has_post_thumbnail()){
+                                            $thumbnail_url = get_the_post_thumbnail_url(null,"large");
+                                            // echo '<a href="'.$thumbnail_url.'" data-featherlight="image">';
+                                            echo '<a class="popup" href="#" data-featherlight="image">';
+                                            the_post_thumbnail("large", array("class" => "img-fluid"));
+                                            echo '</a>';
+                                        }
+                                    the_content();
+                                    ?>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>  
+                <?php
+                    endwhile;
+                ?>
+
+                    <div class="container post-pagination">
+                        <div class="row">
+                            <div class="col-md-4"> </div>
+                            <div class="col-md-8">
+                                <?php 
+                                    the_posts_pagination(array(
+                                        "screen_reader_text"=>' ',
+                                        "prev_text" => "New Posts",
+                                        "next_text" => "Old Posts"
+                                    )); 
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+<?php get_footer(); ?>
