ea5261a4233252b714a346c2e3c713f2ccaff8b2
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-04-28 16:24:40 +0600

    Highlight search results

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -220,3 +220,15 @@ if (!function_exists("alpha_todays_date")) {
         echo date("d/m/y");
     }
 }
+
+function alpha_highlight_search_results($text)
+{
+    if (is_search()) {
+        $pattern = '/('. join('|', explode(' ', get_search_query())).')/i';
+        $text = preg_replace($pattern, '<span class="search-highlight">\0</span>', $text);
+    }
+    return $text;
+}
+add_filter('the_content', 'alpha_highlight_search_results');
+add_filter('the_excerpt', 'alpha_highlight_search_results');
+add_filter('the_title', 'alpha_highlight_search_results');
