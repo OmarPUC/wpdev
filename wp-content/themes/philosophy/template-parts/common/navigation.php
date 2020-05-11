a28386f8f686be94f86bbafd3d6f2323a94ce62d
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-05-11 15:10:23 +0600

    Philosophy: Header navigation menu

---
+++ wp-content/themes/philosophy/template-parts/common/navigation.php
@@ -0,0 +1,19 @@
+<a class="header__toggle-menu" href="#0" title="Menu"><span><?php _e('Menu'); ?></span></a>
+
+<nav class="header__nav-wrap">
+
+    <h2 class="header__nav-heading h6"><?php _e('Site Navigation'); ?></h2>
+
+    <?php
+    wp_nav_menu(
+        array(
+            'theme_location'    =>  'topmenu',
+            'menu_id'   =>  'topmenu',
+            'menu_class'    =>  'header__nav'
+        )
+    );
+    ?>
+
+    <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu"><?php _e('Close'); ?></a>
+
+</nav> <!-- end header__nav-wrap -->
