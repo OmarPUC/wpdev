79b59517ae758d9b89a7cc70a0711efcfb112d86
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-05-13 13:05:50 +0600

    Philosophy: Category Archive and before footer tag section

--- wp-content/themes/philosophy/header.php
+++ wp-content/themes/philosophy/header.php
@@ -30,7 +30,7 @@
             <div class="header__content row">
 
                 <div class="header__logo">
-                    <a class="logo" href="#">
+                    <a class="logo" href="#0">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Homepage">
                     </a>
                 </div> <!-- end header__logo -->
@@ -56,13 +56,17 @@
 
                     <form role="search" method="get" class="header__search-form" action="#">
                         <label>
-                            <span class="hide-content">Search for:</span>
+                            <span class="hide-content">
+                                <?php _e('Search for:', 'philosophy'); ?>
+                            </span>
                             <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off">
                         </label>
                         <input type="submit" class="search-submit" value="Search">
                     </form>
 
-                    <a href="#0" title="Close Search" class="header__overlay-close">Close</a>
+                    <a href="#0" title="Close Search" class="header__overlay-close">
+                        <?php _e('Close', 'philosophy'); ?>
+                    </a>
 
                 </div> <!-- end header__search -->
 
