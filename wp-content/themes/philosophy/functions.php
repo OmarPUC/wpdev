cd6169f984f312bd15febce0529879103154309b
Omar Sharif Ansary <omarsharif439@gmail.com>
2020-05-27 20:38:07 +0600

    Philosophy: Before footer popular posts section

--- wp-content/themes/philosophy/functions.php
+++ wp-content/themes/philosophy/functions.php
@@ -90,5 +90,15 @@ function philosophy_widgets()
         'before_title'  => '<h3 class="quarter-top-margin">',
         'after_title'   => '</h3>',
     ));
+
+    register_sidebar(array(
+        'name'          => __('Before Footer Section', 'philosophy'),
+        'id'            => 'before-footer-right',
+        'description'   =>  __('Before Footer Section, Right Side', 'philosophy'),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ));
 }
 add_action('widgets_init', 'philosophy_widgets');
